e711b6eb46b4b52d9624108be7557faca31aa578
prakhar7080 <prakhargupta3510@gmail.com>
2025-01-02 22:56:49 +0530

    Create printPermutations.cpp

---
+++ printPermutations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+void printPermutation(string &str, int index) {
+	if(index >= str.length() ) {
+		cout << str << " ";
+		return;
+	}
+	for(int j=index;j<str.length(); j++) {
+		swap(str[index], str[j]);
+		printPermutation(str, index+1);
+		swap(str[index], str[j]);
+	}
+	
+}
+
+int main() {
+	string str = "abc";
+	int index = 0;
+	printPermutation(str, index);
+	return 0;
+}
